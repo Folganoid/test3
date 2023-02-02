09e10069507ae70a53f7e9669aca701058cc3b56
Andrii Holubkov <folganoid@yahoo.com>
2023-02-02 13:56:33 +0200

    sdfsdf

--- src/config.jsx
+++ src/config.jsx
@@ -2,7 +2,7 @@ const SETUP = {
         // reactHost: "https://velofg.herokuapp.com",
         // goHost: "https://velofggorest.herokuapp.com"
         reactHost: "https://localhost",
-        goHost: "https://test2-prod-test-2iu7y9.mo4.mogenius.io"
+        goHost: "https://test-prod-test-2iu7y9.mo4.mogenius.io"
     };
 
 export default SETUP
